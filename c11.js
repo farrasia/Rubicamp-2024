813a66c22aafded1abb3f5820fc15d2866b602be
Farras Isfahan Akbar <alfatih1435m@gmail.com>
2024-06-08 15:13:39 +0700

    challange 11

---
+++ c11.js
@@ -0,0 +1,42 @@
+const fs = require('node:fs');
+const readline = require('readline');
+
+let rl = readline.createInterface(process.stdin, process.stdout);
+
+try {
+    const data = fs.readFileSync('data.json','utf8');    
+    daftarPertanyaan = JSON.parse(data);
+} catch (err) {
+    console.log(err);
+}
+
+rl.setPrompt('Tebakan: ');
+let currentQuestionIndex = 0;
+let benar = true;
+
+function askCurrentQuestion(){
+    if(currentQuestionIndex < daftarPertanyaan.length){
+        if(benar){
+            console.log('Pertanyaan: ' + daftarPertanyaan[currentQuestionIndex].definition);
+        }
+        rl.prompt();
+    }  else{
+        console.log("Hore Anda Menang!")
+        rl.close();
+    }
+}
+
+askCurrentQuestion();
+rl.on('line', function(line) {
+    const currentQuestion = daftarPertanyaan[currentQuestionIndex];
+    if(line == currentQuestion.term){
+        console.log("Selamat Anda Benar!\n");
+        benar= true;
+        currentQuestionIndex++;
+        askCurrentQuestion();
+    } else{
+        console.log("Wkwkwkwk Anda Kurang Beruntung!\n");
+        benar = false;
+        askCurrentQuestion();
+    }
+});
